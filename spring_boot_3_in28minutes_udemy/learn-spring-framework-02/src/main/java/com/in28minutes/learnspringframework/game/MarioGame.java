85bd682bed1996751883601c1665527e497f98a6
Kundan Kumar <rrd51510@gmail.com>
2023-11-18 12:27:06 +0530

    @Component @ComponentScan

---
+++ spring_boot_3_in28minutes_udemy/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
@@ -0,0 +1,20 @@
+package com.in28minutes.learnspringframework.game;
+
+public class MarioGame implements GamingConsole {
+
+    public void up() {
+        System.out.println("MarioGame - UP");
+    }
+
+    public void down() {
+        System.out.println("MarioGame - DOWN");
+    }
+
+    public void left() {
+        System.out.println("MarioGame - LEFT");
+    }
+
+    public void right() {
+        System.out.println("MarioGame - RIGHT");
+    }
+}
