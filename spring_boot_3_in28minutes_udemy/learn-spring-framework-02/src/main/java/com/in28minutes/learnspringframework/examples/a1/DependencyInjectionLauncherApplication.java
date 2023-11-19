a01d55d3a6c154f47f389022b2c23217babcc2ec
Kundan Kumar <rrd51510@gmail.com>
2023-11-19 09:19:05 +0530

    real world example

--- spring_boot_3_in28minutes_udemy/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DependencyInjectionLauncherApplication.java
+++ spring_boot_3_in28minutes_udemy/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DependencyInjectionLauncherApplication.java
@@ -1,4 +1,4 @@
-package com.in28minutes.learnspringframework;
+package com.in28minutes.learnspringframework.examples.a1;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GamingConsole;
@@ -6,14 +6,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 @Configuration
-@ComponentScan("com.in28minutes.learnspringframework.game")
-public class GamingAppLauncherApplication {
+@ComponentScan
+public class DependencyInjectionLauncherApplication {
     public static void main(String[] args) {
 
-        try(var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
-            context.getBean(GamingConsole.class).up();
-            context.getBean(GameRunner.class).run();
+        try(var context = new AnnotationConfigApplicationContext(DependencyInjectionLauncherApplication.class)) {
+
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
         }
     }
 
